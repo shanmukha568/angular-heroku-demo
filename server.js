47ea8137437e76d651194c7ddb84fa5d94494cd4
AdhiKotcherla <adhisaiganesh@example.com>
2024-01-19 10:08:50 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static('./dist/angular-heroku-demo'));
+app.get('/*',(req,res)=>
+    res.sendFile('index.html',{root:'dist/angular-heroku'}),
+);
+
+app.listen(process.env.PORT || 8080);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-heroku-demo';
+  title = 'Adhi angular-heroku-demo Project';
 }
